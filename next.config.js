8b3217f937aa93d4aa6439579ec029804c4a6f64
mayanhavoc <mayanhavoc@gmail.com>
2023-04-28 17:19:22 -0400

    add webpack config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	webpack: (config, { isServer }) => {
+		config.module.rules.push({
+			test: /\.md$/,
+			use: 'raw-loader',
+		});
 
-module.exports = nextConfig
+		if (!isServer) {
+			config.resolve.fallback.fs = false;
+		}
+
+		return config;
+	},
+};
+
+module.exports = nextConfig;
