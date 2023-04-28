9ed359392189231d37cbf6b73514fca6f81ac0f1
mayanhavoc <mayanhavoc@gmail.com>
2023-04-28 17:32:10 -0400

    move li element to follow map to satisfy iterator key rule

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -13,22 +13,20 @@ export default function Modules({ posts }) {
 				<h2 className='text-4xl font-bold dark:text-white/90'>Módulos</h2>
 				<ul className='w-full'>
 					{posts.map((post) => (
-						<div className='my-5'>
-							<div className='grid grid-cols-1 md:grid-cols-2 gap-4 my-5 max-w-2xl'>
-								<div className='bg-white shadow-md rounded-md overflow-hidden'>
-									<li
-										key={post.slug}
-										className='mt-4 text-2xl dark:text-white/90'>
+						<li key={post.slug} className='mt-4 text-2xl dark:text-white/90'>
+							<div className='my-5'>
+								<div className='grid grid-cols-1 md:grid-cols-2 gap-4 my-5 max-w-2xl'>
+									<div className='bg-white shadow-md rounded-md overflow-hidden'>
 										<div className='p-4'>
 											<h3 className='text-lg font-bold mb-2'>{post.title}</h3>
 											<button className='rounded-md bg-gray-800 p-3 text-white text-sm'>
 												<Link href={`/blog/${post.slug}`}>Leer más</Link>
 											</button>
 										</div>
-									</li>
+									</div>
 								</div>
 							</div>
-						</div>
+						</li>
 					))}
 				</ul>
 			</section>
