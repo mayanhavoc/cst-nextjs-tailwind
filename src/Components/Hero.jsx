518ef43e5e2d8b686b7a1c5d32d2e8656d54c663
mayanhavoc <mayanhavoc@gmail.com>
2023-05-03 11:38:05 -0400

    testing local image upload

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,9 @@
 import Image from 'next/image';
-import logo from '/cst-logo.png';
 
 const Hero = () => {
 	return (
 		<div className='hero-div'>
-			<Image src={logo} alt='logo' width={300} height={300} />
+			<Image src='/cst-logo.png' alt='logo' width={300} height={300} />
 			<h1 className='text-center text-4xl font-bold'>
 				Cardano Sin Tecnicismos
 			</h1>
